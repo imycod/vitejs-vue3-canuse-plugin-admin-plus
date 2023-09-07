25cdfa6a78e01cb9d99085710e1555045d8b8d74
wuxs <317009160@qq.com>
2023-09-07 16:49:39 +0800

    modify:用@apply组合多个class类

--- src/views/Layout/Table/Index1.vue
+++ src/views/Layout/Table/Index1.vue
@@ -2,7 +2,7 @@
  * @Author: wuxs 317009160@qq.com
  * @Date: 2023-08-10 12:57:27
  * @LastEditors: wuxs 317009160@qq.com
- * @LastEditTime: 2023-09-07 16:21:52
+ * @LastEditTime: 2023-09-07 16:35:21
  * @FilePath: \ehr-console-pcd:\studio\vue-project\vue3-plugin-effect-vite-admin-element-plus\src\views\Directive\Disabled.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -118,7 +118,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="layout-container layout-theme-view layout-flex-full-view layout-padding-auto">
+  <div class="layout-full-col">
     <Search :schema="allSchemas.searchSchema" @search="setSearchParams" @reset="setSearchParams" />
 
     <div class="mb-10px">
